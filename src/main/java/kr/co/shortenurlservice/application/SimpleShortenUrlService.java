88cb5bf31d52d8d9c793b351f14b9f168df32e67
tngh4037 <tngh4037@gmail.com>
2024-07-07 16:50:36 +0900

    테스트 코드에서 오류가 발생하도록 코드 수정 (2)

--- src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
+++ src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
@@ -80,7 +80,9 @@ public class SimpleShortenUrlService {
         }
 
     //    throw new LackOfShortenUrlKeyException();
-        throw new RuntimeException();
+    //    throw new RuntimeException();
+
+        throw new IllegalArgumentException();
     }
 
 }
